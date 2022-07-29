994e1f7f668e88cff57383d2cf3f2115c6289f92
deboahemaa <78213392+deboahemaa@users.noreply.github.com>
2022-07-29 15:57:59 +0200

    Update refl.py

--- refl.py
+++ refl.py
@@ -1,23 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy
 
-df = open("raw_data_refl_def.csv").read().split("\n")
+df1 = open("watercorr_txt.txt").read().split("\n")
+#df2 = open("reflectiont3conc19.txt").read().split("\n")
+#df3 = open("reflectiont3conc31.txt").read().split("\n")
+#df4 = open("reflectiont0conc01.txt").read().split("\n")
 
-refl_list = []
+refl_list_1 = []
+refl_list_2 = []
+refl_list_3 = []
+refl_list_4 = []
 wl_list = []
 
-print(df[1].split(" "))
-for i in range(len(df)):
-    a, b = df[i].split(" ")
-    refl_list.append(a)
+for i in range(len(df1)):
+    refl_list_1.append(df1[i])
+#    refl_list_2.append(df2[i])
+#    refl_list_3.append(df3[i])
+#    refl_list_4.append(df4[i])
 
+refl_list_1 = [float(e) for e in refl_list_1]
+#refl_list_2 = [float(e) for e in refl_list_2]
+#refl_list_3 = [float(e) for e in refl_list_3]
+#refl_list_4 = [float(e) for e in refl_list_4]
 
-refl_list = [float(e) for e in refl_list]
-print(refl_list)
-
-for wl in range(455,700, 5):
+for wl in range(380,781, 5):
     wl_list.append(wl)
 
 plt.ylim(0.2, 1)
-plt.plot(wl_list, refl_list)
+plt.plot(wl_list, refl_list_1, label='1% mel')
+#plt.plot(wl_list, refl_list_2, '.', label='19% mel')
+#plt.plot(wl_list, refl_list_3,'+', label='31% mel')
+#plt.plot(wl_list, refl_list_4, '--', label='125 hrs')
+plt.legend()
+plt.ylabel('Reflectance R (a.u.)')
+plt.xlabel('Wavelength (nm)')
 plt.show()
